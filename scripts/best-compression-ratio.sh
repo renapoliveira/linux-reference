06cb6ee8023ee124ee2cdbcf296013ce68d4c8ff
Rena <rena.p.oliveira@gmail.com>
2018-04-06 14:46:21 +0000

    -Removing compressed files before starting the script -Informing user which compressing applications are not installed

--- scripts/best-compression-ratio.sh
+++ scripts/best-compression-ratio.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #This script will compress a file or directory in multiple compression types and keep the generated file with the best compression ratio
 
-
 INPUT=$1
 OUTPUT="compressed."
+
+rm -rf compressed.*
 
 if [ -z ${INPUT} ]
 then
@@ -22,46 +23,56 @@ else
     exit 1
 fi
 
+
+#TAR
 if [ `command -v tar` ]
 then
     if [ `command -v gzip` ]
     then
-        #gunzip with best compression rate
+        #GUNZIP (with best compression rate)
         EXT="tar"
         echo "Compressing gunzip..."
-        tar -cf ${OUTPUT}${EXT} $INPUT ; gzip --best ${OUTPUT}${EXT}
+        tar -cf ${OUTPUT}${EXT} $INPUT ; gzip --best -f ${OUTPUT}${EXT}
         echo ${OUTPUT}${EXT}".gz size is" `find -name ${OUTPUT}${EXT}.gz -printf '%s'`
+    else
+        echo "GUNZIP is not installed"
     fi
 
-    #bzip2
+    #BZIP2
     EXT="tar.bz2"
     echo "Compressing bzip2..."
     tar -cjf ${OUTPUT}${EXT} ${INPUT}
     echo ${OUTPUT}${EXT}" size is" `find -name ${OUTPUT}${EXT} -printf '%s'`
 
-    #xz (replacing lzma https://tukaani.org/lzma/)
+    #XZ (replacing lzma https://tukaani.org/lzma/)
     EXT="tar.xz"
     echo "Compressing xz..."
     tar -cJf ${OUTPUT}${EXT} ${INPUT}
     echo ${OUTPUT}${EXT}" size is" `find -name ${OUTPUT}${EXT} -printf '%s'`
+else
+    echo "TAR is not installed"
 fi
 
 if [ `command -v rar` ]
 then
-    #rar
+    #RAR
     EXT="rar"
     echo "Compressing rar..."
     rar a ${PARAM} -inul ${OUTPUT}${EXT} ${INPUT}
     echo ${OUTPUT}${EXT}" size is" `find -name ${OUTPUT}${EXT} -printf '%s'`
+else
+    echo "RAR is not installed"
 fi
 
 if [ `command -v 7z` ]
 then
-    #7z
+    #7Z
     EXT="7z"
     echo "Compressing 7z..."
     7z a compressed.7z ${OUTPUT}${EXT} ${INPUT} &> /dev/null
     echo ${OUTPUT}${EXT}" size is" `find -name ${OUTPUT}${EXT} -printf '%s'`
+else
+    echo "7Z is not installed"
 fi
 
 if [ `command -v zip` ]
@@ -72,9 +83,11 @@ then
     7z a ${OUTPUT}${EXT} ${INPUT} &> /dev/null
     zip -q ${PARAM} compressed.zip ${INPUT}
     echo ${OUTPUT}${EXT}" size is" `find -name ${OUTPUT}${EXT} -printf '%s'`
+else
+    echo "ZIP is not installed"
 fi
 
-SMALLER=`find . -maxdepth 1 -name "compressed*" -printf '%s %p\n' | sort -n | head -n 1 | cut -d " " -f 2`
-echo ${SMALLER} "has the best compression ratio, keeping it and removing the other ones."
+SMALLER=`find . -maxdepth 1 -name "compressed.*" -printf '%s %p\n' | sort -n | head -n 1 | cut -d " " -f 2`
 
-find . -maxdepth 1 -name "compressed*" -printf '%p\n' | grep -v ${SMALLER} | xargs rm -f
+echo ""; echo ${SMALLER} "presented the best compression ratio, keeping it and removing the other ones."; echo ""
+find . -maxdepth 1 -name "compressed.*" -printf '%p\n' | grep -v ${SMALLER} | xargs rm -f
